d20d51f08c67e200b1e99bcd06427df9b6a1eac3
A251022 <A251022@INBLRWIT251022>
2017-06-14 12:38:49 +0530

    Add project files.

---
+++ GitIntegration-Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitIntegration_Test.Startup))]
+namespace GitIntegration_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
